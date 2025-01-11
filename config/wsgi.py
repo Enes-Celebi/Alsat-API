7773c2eb6fde9fa28c0dca42e6697a06c0668650
Enes-Celebi <121902027+Enes-Celebi@users.noreply.github.com>
2025-01-11 20:53:45 +0300

    configured wsgi to find .env

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,9 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
-import dotenv
 
-dotenv.load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
